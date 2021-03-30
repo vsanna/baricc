940c57c8ebd83aa30b4cb01a9d768aa3cc5dade1
ryu.ishikawa <ryu.ishikawa@smartnews.com>
2021-03-30 16:26:58 +0900

    feat: add test script for skelton

--- REAEDME.md
+++ REAEDME.md
@@ -1,2 +1,12 @@
 docker-compose up -d
 docker-compose exec app bash
+
+# アセンブラを渡す
+cc -o main main.c
+./main 123
+cc -o tmp tmp.s
+./tmp
+echo $?
+
+# テスト
+bash -x ./test.sh
--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
+    printf("main:\n");
     printf("  mov rax, %d\n", atoi(argv[1]));
     printf("  ret\n");
     return 0;

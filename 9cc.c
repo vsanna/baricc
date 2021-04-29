a6a70b6b0a6243032e34462a8471f472fe3397ed
ryu.ishikawa <ryu.ishikawa@smartnews.com>
2021-04-30 03:55:23 +0900

    feat: impl tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,45 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+
+// tokenの種類
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF       // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // トークンの種別
+    Token *next;    // 次の入力トークン
+    int val;        // TK_NUMの場合の値
+    char *str;      // トークン文字列
+};
+
+// 現在見ているtoken
+Token* token;
+
+// エラーをstderrに出すためのutil 関数
+void error(char *fmt, ...) {
+    // TODO: なにこれ
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// 次のトークンが期待している記号のときにはトークンを一つ読み進めてtrueを返す
+// それ以外にはfalseを返す
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+// 次のトークンが期待している記号のときにはトークンを一つ読み進める。
+// それ以外にはエラーを投げる
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error("'%c'ではありません", op);
+    }
+    token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを一つ読み進めてその数値を返す
+// それ以外の場合にはエラー
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error("数ではありませんん");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurのnextにセット
+// ?"+ 12 - hoge" が渡ってきたとき、tok->str = str で先頭の1文字だけ渡るのはなぜ
+//   -> わかった。その位置のアドレスを保持しているのみで、ちゃんとsplitしているわけではない...
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    // callocは割り当てられたメモリをゼロクリアしてくれる
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str; // charだけをsetしてる?
+    cur->next = tok;
+    return tok;
+}
+
+// 入力文字列pをトークない頭してそれを返す
+// headはコードを簡単にするためのtrick. ポインターでlinkedlistを作るときの定石らしい.
+Token* tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while(*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p);
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            // 数字分だけ前に進む
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "invalid num of arguments");
         return 1;
     }
 
-    // *(ptr) -> actual value
-    // &(val) -> pointer
-    // &(&val) -> ptr of ptr.
-    // cではすべて値渡しなので大きなデータはptrわたしをする.
-    // また、返り値が1つしかもてないので複数返り値を持ちたい場合もptrを渡す
-    // また、callerが持つ変数を関数内部で書き換えたい場合もptr渡しをする
-    char* p = argv[1];
+    token = tokenize(argv[1]);
 
-
-    // 複数あるアセンブリ気泡の中でintel気泡を選択
+    // アセンブリの前半を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    // strtol(文字列(charのptr), 変換失敗時に変換できなかった文字列を格納するためのptrのptr, 基数)
-    // 指定した文字列の先頭にある数字の部分をlongとして取得. また
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-    while(*p) {
-        if (*p == '+') {
-            // pointerを一つ次のアドレスにすすめる
-            p++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
+    // 式の最初は数でないと行けない
+    // 最初のmov命令を出力
+    // この時点ではtoken.kind == TK_NUM
+    printf("  mov rax, %d\n", expect_number());
+    // この時点ではexpect_number()の内部処理により次のtokenに移行済み
+    // token.kind == TK_RESERVED.
+
+    while(!at_eof()) {
+        if (consume('+')) {
+            printf("  add rax, %d\n", expect_number());
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "invalid characters: %c\n", *p);
-        return 1;
+        expect('-');
+        printf("  sub rax, %d\n", expect_number());
     }
 
     printf("  ret\n");

4e0073063878f6e65ac5cc7f6d39eaa731f55741
ryu.ishikawa <ryu.ishikawa@smartnews.com>
2021-04-30 05:07:56 +0900

    feat: add, sub, mul, div

--- 9cc.c
+++ 9cc.c
@@ -101,12 +101,13 @@ Token* tokenize() {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p);
             p++;
             continue;
         }
 
+        // TODO: 今の箇所からどれだけの長さがdigitなのか、についてどう判断するのか
         if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
             // 数字分だけ前に進む
@@ -121,6 +122,133 @@ Token* tokenize() {
     return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;   // kind == ND_NUMの場合のみ使う
+};
+
+Node* new_node(NodeKind kind, Node* lhs, Node* rhs) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+};
+
+// 数字のnodeを作る特殊
+Node* new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    // lhs, rhsはnull
+    return node;
+}
+
+Node* primary();
+Node* mul();
+Node* expr();
+
+// この3つをparseできれば構文解析が出来るという前提
+// expr    = mul("+" mul | "-" mul)*
+// mul     = primary("*" primary | "/" primary)
+// primary = num | "(" num ")"
+// ここでは expr    = mul("+" mul | "-" mul)* をコードに落とし込んで切る
+Node* expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node* mul() {
+    Node* node = primary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node* primary() {
+    // 次のトークンが ( なら ( expr ) のハズ
+    if (consume('(')) {
+        Node* node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければnumのハズ
+    return new_node_num(expect_number());
+}
+
+
+// 構文木からアセンブラを作るところまで一気に進める
+void gen(Node* node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch(node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            /*
+            idiv: 符号あり除算.
+            暗黙的にrdx, raxをとってそれを合わせたものを128bit整数とみなしてそれを引数レジスタの64bitでわり、
+            商をraxに、あまりをrdxにセットする
+            cqo命令を使うとraxに入っている64bitの値を128bitに伸ばしてrdx, raxにセットすることが出来る
+
+            pop rdi   // stackから取り出して割る数としてset
+            pop rax   // stackから取り出して割られる数としてset
+            cqo       // rdx, rax <- 10 を代入する命令. 全体で10なので、rdxは0が入る
+            idiv rdi  // rdx,rax(10) / rdi を行い、商をrax, あまりをrdxにset
+            push rax  // 答えをpush
+            */
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
 
 
 int main(int argc, char **argv) {
@@ -130,29 +258,16 @@ int main(int argc, char **argv) {
     }
 
     user_input = argv[1];
-    token = tokenize(argv[1]);
+    token = tokenize();
+    Node* node = expr();
 
     // アセンブリの前半を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // 式の最初は数でないと行けない
-    // 最初のmov命令を出力
-    // この時点ではtoken.kind == TK_NUM
-    printf("  mov rax, %d\n", expect_number());
-    // この時点ではexpect_number()の内部処理により次のtokenに移行済み
-    // token.kind == TK_RESERVED.
-
-    while(!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
+    gen(node);
+    printf("  pop rax\n");
 
     printf("  ret\n");
     return 0;
